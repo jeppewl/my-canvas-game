1415ca0de79e35802040e6daa35c25ef5065f3f0
jeppewl <46894168+jeppewl@users.noreply.github.com>
2025-06-02 15:40:28 +0200

    Till Game Loop 35

--- src/main.js
+++ src/main.js
@@ -1,17 +1,53 @@
 import { resources } from "./Resource.js";
+import { Sprite } from "./Sprite.js";
 import "./style.css";
+import { Vector2 } from "./Vector2.js";
 
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
 
+const skySprite = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180),
+});
+
+const groundSprite = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180),
+});
+
+const heroSprite = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1,
+});
+
+const shadowSprite = new Sprite({
+  resource: resources.images.shadow,
+  frameSize: new Vector2(32, 32),
+});
+
+const heroPos = new Vector2(16 * 5, 16 * 5);
+
 const draw = () => {
-  const sky = resources.images.sky;
-  if (sky.isLoaded) {
-    ctx.drawImage(sky.image, 0, 0);
-  }
+  skySprite.drawImage(ctx, 0, 0);
+  groundSprite.drawImage(ctx, 0, 0);
+
+  // Center the Hero in the cell
+  const hero0ffset = new Vector2(+8, -21);
+  const heroPosX = heroPos.x + hero0ffset.x;
+  const heroPosY = heroPos.y + 1 + hero0ffset.y;
+
+  shadowSprite.drawImage(ctx, heroPosX, heroPosY);
+  // shadow.drawImage(ctx, heroPos.x, heroPos.y);
+  heroSprite.drawImage(ctx, heroPosX, heroPosY);
+  // heroSprite.drawImage(ctx, heroPos.x, heroPos.y);
 };
 
 setInterval(() => {
-  console.log("draw...");
+  // heroSprite.frame += 1;
+  // console.log("draw...");
   draw();
 }, 300);
